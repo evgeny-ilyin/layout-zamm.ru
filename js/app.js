138b8e5ad6c0067dfed5fbce793123c2633aa2a1
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2023-09-06 18:49:09 +0300

    Update 2023-09-06T15:49:06.997Z

---
+++ js/app.js
@@ -0,0 +1,484 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/modules/functions.js
+function stickyHeader() {
+	const header = document.querySelector("header");
+
+	let handleScroll = () => {
+		if (window.scrollY > 0) {
+			header.classList.add("header_fixed");
+		} else {
+			header.classList.remove("header_fixed");
+		}
+	};
+	window.addEventListener("scroll", handleScroll);
+	handleScroll();
+}
+
+function mobileCatalog() {
+	const navMenu = document.querySelector(".nav__menu"),
+		menuToggler = document.getElementById("menu-toggle"),
+		subMenuWrapper = document.querySelector(".catalog__submenu"),
+		catalogLinks = document.querySelectorAll(".catalog__list .catalog__link"),
+		backBtn = document.querySelector(".nav__back"),
+		isActiveClass = "is-active",
+		hideNavClass = "hide-nav";
+
+	catalogLinks.forEach((catalogLink) => {
+		catalogLink.addEventListener("click", (e) => {
+			const category = catalogLink.nextElementSibling;
+			if (category) {
+				e.preventDefault();
+				const cloneCategory = category.cloneNode(true);
+				cloneCategory.classList.add(isActiveClass);
+				subMenuWrapper.classList.add(isActiveClass);
+				subMenuWrapper.innerHTML = "";
+				subMenuWrapper.append(cloneCategory);
+				backBtn.classList.add(isActiveClass);
+			}
+		});
+	});
+
+	backBtn.addEventListener("click", () => {
+		backBtn.classList.remove(isActiveClass);
+		subMenuWrapper.classList.remove(isActiveClass);
+		setTimeout(() => {
+			subMenuWrapper.innerHTML = "";
+		}, 250);
+	});
+
+	menuToggler.addEventListener("change", () => {
+		if (!this.checked) {
+			backBtn.classList.remove(isActiveClass);
+			subMenuWrapper.classList.remove(isActiveClass);
+			navMenu.classList.remove(hideNavClass);
+			subMenuWrapper.innerHTML = "";
+		}
+	});
+
+	window.addEventListener("resize", () => {
+		if (menuToggler.checked) {
+			menuToggler.click();
+		}
+	});
+}
+
+// не используются >
+function isTouchEnabled() {
+	return "ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+}
+
+function dropdownByTouch() {
+	if (!isTouchEnabled()) {
+		return;
+	}
+
+	const dropdownTouch = document.querySelectorAll(".dropdown-touch"),
+		isActiveClass = "is-active";
+
+	dropdownTouch.forEach((link) => {
+		link.addEventListener("click", (e) => {
+			e.stopPropagation();
+			const dropdown = link.nextElementSibling;
+			if (!dropdown.classList.contains(isActiveClass)) {
+				dropdownClose(e);
+			}
+			dropdown.classList.toggle(isActiveClass);
+		});
+	});
+
+	document.addEventListener("click", (e) => {
+		dropdownClose(e);
+	});
+
+	let dropdownClose = (e) => {
+		const dropdownTouchActive = document.querySelectorAll(`.dropdown-touch + .${isActiveClass}`);
+		if (dropdownTouchActive) {
+			dropdownTouchActive.forEach((dropdown) => {
+				if (!dropdown.contains(e.target)) {
+					dropdown.classList.remove(isActiveClass);
+				}
+			});
+		}
+	};
+}
+// не используются <
+
+function searchForm() {
+	const navMenu = document.querySelector(".nav__menu"),
+		menuToggler = document.getElementById("menu-toggle"),
+		searchForm = document.querySelector(".nav__search"),
+		searchInput = document.querySelector(".input_nav-search"),
+		closeSearchBtn = document.querySelector(".close-search"),
+		isActiveClass = "is-active",
+		hideNavClass = "hide-nav";
+
+	searchInput.addEventListener("focus", () => {
+		searchForm.classList.add(isActiveClass);
+
+		if (menuToggler.checked) {
+			navMenu.classList.add(hideNavClass);
+		}
+	});
+
+	closeSearchBtn.addEventListener("click", () => {
+		searchForm.classList.remove(isActiveClass);
+		navMenu.classList.remove(hideNavClass);
+	});
+
+	document.addEventListener("click", (e) => {
+		if (!searchForm.contains(e.target)) {
+			if (!menuToggler.checked) {
+				searchForm.classList.remove(isActiveClass);
+			}
+		}
+	});
+}
+
+function closeModal() {
+	const closeButtons = document.querySelectorAll(".btn_close"),
+		isActiveClass = "is-active";
+	closeButtons.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			const modal = btn.closest(`.${isActiveClass}`);
+			if (modal) {
+				modal.classList.remove(isActiveClass);
+			}
+		});
+	});
+}
+
+function accordion() {
+	const accordionFooter = document.querySelectorAll(".f-menu.js-accordion"),
+		accordionTriggers = document.querySelectorAll(".js-accordion__trigger"),
+		isActiveClass = "is-active",
+		isEnabledClass = "on";
+
+	accordionTriggers.forEach((trigger) => {
+		trigger.addEventListener("click", () => {
+			const accordionParent = trigger.parentElement,
+				accordionContent = trigger.nextElementSibling;
+
+			if (accordionParent.classList.contains(isEnabledClass)) {
+				trigger.classList.toggle(isActiveClass);
+				if (accordionContent.style.maxHeight) {
+					accordionContent.style.maxHeight = null;
+				} else {
+					accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+				}
+			}
+		});
+	});
+
+	let accordionBuildFooter = () => {
+		accordionFooter.forEach((el) => {
+			el.classList.add(isEnabledClass);
+		});
+	};
+
+	let accordionDestroyFooter = () => {
+		accordionFooter.forEach((el) => {
+			el.classList.remove(isEnabledClass);
+		});
+	};
+
+	let mobileCheck = (mq) => {
+		if (mq.matches) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	const mq = window.matchMedia("(max-width: 767px)");
+	let timeout;
+
+	["load", "resize"].forEach((evt) =>
+		window.addEventListener(evt, () => {
+			if (!timeout) {
+				timeout = setTimeout(function () {
+					// Reset timeout
+					timeout = null;
+					const isMobile = mobileCheck(mq);
+					if (isMobile) {
+						accordionBuildFooter();
+					} else if (!isMobile) {
+						accordionDestroyFooter();
+					}
+				}, 200);
+			}
+		})
+	);
+}
+
+;// CONCATENATED MODULE: ./src/js/modules/dynamicAdapt.js
+/**
+ * @typedef {Object} dNode
+ * @property {HTMLElement} parent
+ * @property {HTMLElement} element
+ * @property {HTMLElement} to
+ * @property {string} breakpoint
+ * @property {string} order
+ * @property {number} index
+ */
+
+/**
+ * @typedef {Object} dMediaQuery
+ * @property {string} query
+ * @property {number} breakpoint
+ */
+
+/**
+ * @param {'min' | 'max'} type
+ */
+function useDynamicAdapt(type = 'max') {
+  const className = '_dynamic_adapt_'
+  const attrName = 'data-da'
+
+  /** @type {dNode[]} */
+  const dNodes = getDNodes()
+
+  /** @type {dMediaQuery[]} */
+  const dMediaQueries = getDMediaQueries(dNodes)
+
+  dMediaQueries.forEach((dMediaQuery) => {
+    const matchMedia = window.matchMedia(dMediaQuery.query)
+    // массив объектов с подходящим брейкпоинтом
+    const filteredDNodes = dNodes.filter(({ breakpoint }) => breakpoint === dMediaQuery.breakpoint)
+    const mediaHandler = getMediaHandler(matchMedia, filteredDNodes)
+    matchMedia.addEventListener('change', mediaHandler)
+
+    mediaHandler()
+  })
+
+  function getDNodes() {
+    const result = []
+    const elements = [...document.querySelectorAll(`[${attrName}]`)]
+
+    elements.forEach((element) => {
+      const attr = element.getAttribute(attrName)
+      const [toSelector, breakpoint, order] = attr.split(',').map((val) => val.trim())
+
+      const to = document.querySelector(toSelector)
+
+      if (to) {
+        result.push({
+          parent: element.parentElement,
+          element,
+          to,
+          breakpoint: breakpoint ?? '767',
+          order: order !== undefined ? (isNumber(order) ? Number(order) : order) : 'last',
+          index: -1,
+        })
+      }
+    })
+
+    return sortDNodes(result)
+  }
+
+  /**
+   * @param {dNode} items
+   * @returns {dMediaQuery[]}
+   */
+  function getDMediaQueries(items) {
+    const uniqItems = [...new Set(items.map(({ breakpoint }) => `(${type}-width: ${breakpoint}px),${breakpoint}`))]
+
+    return uniqItems.map((item) => {
+      const [query, breakpoint] = item.split(',')
+
+      return { query, breakpoint }
+    })
+  }
+
+  /**
+   * @param {MediaQueryList} matchMedia
+   * @param {dNodes} items
+   */
+  function getMediaHandler(matchMedia, items) {
+    return function mediaHandler() {
+      if (matchMedia.matches) {
+        items.forEach((item) => {
+          moveTo(item)
+        })
+
+        items.reverse()
+      } else {
+        items.forEach((item) => {
+          if (item.element.classList.contains(className)) {
+            moveBack(item)
+          }
+        })
+
+        items.reverse()
+      }
+    }
+  }
+
+  /**
+   * @param {dNode} dNode
+   */
+  function moveTo(dNode) {
+    const { to, element, order } = dNode
+    dNode.index = getIndexInParent(dNode.element, dNode.element.parentElement)
+    element.classList.add(className)
+
+    if (order === 'last' || order >= to.children.length) {
+      to.append(element)
+
+      return
+    }
+
+    if (order === 'first') {
+      to.prepend(element)
+
+      return
+    }
+
+    to.children[order].before(element)
+  }
+
+  /**
+   * @param {dNode} dNode
+   */
+  function moveBack(dNode) {
+    const { parent, element, index } = dNode
+    element.classList.remove(className)
+
+    if (index >= 0 && parent.children[index]) {
+      parent.children[index].before(element)
+    } else {
+      parent.append(element)
+    }
+  }
+
+  /**
+   * @param {HTMLElement} element
+   * @param {HTMLElement} parent
+   */
+  function getIndexInParent(element, parent) {
+    return [...parent.children].indexOf(element)
+  }
+
+  /**
+   * Функция сортировки массива по breakpoint и order
+   * по возрастанию для type = min
+   * по убыванию для type = max
+   *
+   * @param {dNode[]} items
+   */
+  function sortDNodes(items) {
+    const isMin = type === 'min' ? 1 : 0
+
+    return [...items].sort((a, b) => {
+      if (a.breakpoint === b.breakpoint) {
+        if (a.order === b.order) {
+          return 0
+        }
+
+        if (a.order === 'first' || b.order === 'last') {
+          return -1 * isMin
+        }
+
+        if (a.order === 'last' || b.order === 'first') {
+          return 1 * isMin
+        }
+
+        return 0
+      }
+
+      return (a.breakpoint - b.breakpoint) * isMin
+    })
+  }
+
+  function isNumber(value) {
+    return !isNaN(value)
+  }
+}
+
+;// CONCATENATED MODULE: ./src/js/modules/cookies.js
+/**
+ * Cookie policy
+ * @link https://learn.javascript.ru/cookie
+ */
+function getCookie(name) {
+	let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, days = 1) {
+	let expires;
+
+	if (days) {
+		let date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = "; expires=" + date.toUTCString();
+	}
+
+	let options = {
+		path: "/",
+		expires: expires,
+		// defaults
+	};
+
+	if (options.expires instanceof Date) {
+		options.expires = options.expires.toUTCString();
+	}
+
+	let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+	for (let optionKey in options) {
+		updatedCookie += "; " + optionKey;
+		let optionValue = options[optionKey];
+		if (optionValue !== true) {
+			updatedCookie += "=" + optionValue;
+		}
+	}
+
+	document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+	setCookie(name, "", {
+		"max-age": -1,
+	});
+}
+
+const cookieForm = document.querySelector(".cookie"),
+	cookieAccept = document.querySelector(".cookie__accept");
+
+let policyCheck = () => {
+	if (!getCookie("policyAccepted")) {
+		cookieForm.classList.remove("hidden");
+	}
+};
+
+let policyAccepted = (e) => {
+	e.preventDefault();
+	setCookie("policyAccepted", "1", 7);
+	cookieForm.classList.add("hidden");
+};
+
+cookieAccept.addEventListener("click", policyAccepted);
+window.addEventListener("load", policyCheck);
+
+;// CONCATENATED MODULE: ./src/js/app.js
+
+
+
+
+
+addEventListener("DOMContentLoaded", () => {
+	// functions.stickyHeader();
+	// functions.isTouchEnabled();
+	// functions.dropdownByTouch();
+	mobileCatalog();
+	searchForm();
+	closeModal();
+	accordion();
+	useDynamicAdapt();
+});
+
+/******/ })()
+;
