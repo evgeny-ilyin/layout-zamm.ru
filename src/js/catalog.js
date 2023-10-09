2fd88e3a9c6ff2643e457a69487da55d49daf534
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2023-10-09 10:23:21 +0300

    solid js for each page

--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,13 +1,42 @@
+import * as functions from "./modules/functions.js";
+import * as fancyapps from "./modules/fancyapps.js";
+import { productGalleriesInit, productGallery, productFavourite } from "./modules/product.js";
+import { useDynamicAdapt } from "./modules/dynamicAdapt.js";
+import "./modules/cookies.js";
+
+// catalog
 import { productPropsHover, productProps, productFetches, filterTagsSet, filterTagsRemove } from "./modules/product.js";
 import { rangeSlidersInit } from "./modules/nouislider.js";
 
 addEventListener("DOMContentLoaded", () => {
-	rangeSlidersInit();
+	useDynamicAdapt();
+	
+	functions.stickyHeader();
+	functions.mobileCatalog();
+	functions.searchForm();
+	functions.sectionClose();
+	functions.sectionShow();
+	functions.dropdownClose();
+	functions.dropdownShow();
+	functions.overlayClick();
+	functions.collapseHandler();
+	functions.accordion();
+	functions.accordionFooter();
+	functions.ideaMarkerShow();
+	functions.ideaPopupPlace();
 
+	fancyapps.carouselsInit();
+
+	productGalleriesInit();
+	productGallery();
+	productFavourite();
+
+
+	// catalog
+	rangeSlidersInit();
 	productPropsHover();
 	productProps();
 	productFetches();
-
 	filterTagsSet();
 	filterTagsRemove();
 });
