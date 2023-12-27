5e7a212f91352d2fb27c076dde7f1e87670d9e92
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2023-12-28 02:50:11 +0300

    new common functions

--- src/js/common.js
+++ src/js/common.js
@@ -1,18 +1,26 @@
 import "./modules/common-global.js";
-import * as fn from "./modules/common.js";
 import { useDynamicAdapt } from "./modules/common-dynamicAdapt.js";
+import { fileInputInit } from "./modules/form-custom-file-input.js";
+import "./modules/form-custom-select.js";
+import * as fn from "./modules/common.js";
+import * as form from "./modules/form-submit.js";
 import "./modules/common-cookies.js";
 
 addEventListener("DOMContentLoaded", () => {
 	isTouchDevice();
+	showHidden();
 	catalogItemGalleriesInit();
 	catalogItemGalleryHandler();
 	useDynamicAdapt();
+	setFavourites();
+	selectsInit();
+	fileInputInit();
 
 	fn.stickyHeader();
 	fn.hamburgerMenu();
 	fn.modalHandler();
 	fn.sectionClose();
+	fn.collapseHandler();
 	fn.searchForm();
 	fn.inputFetch();
 	fn.inputQuickSearch();
@@ -20,4 +28,9 @@ addEventListener("DOMContentLoaded", () => {
 	fn.accordionFooter();
 	fn.dropdownClose();
 	fn.dropdownShow();
+	fn.contentGalleryPopup();
+	fn.changeAmount();
+
+	form.submitPrevent();
+	form.maskHandler();
 });
