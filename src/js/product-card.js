54b8a6b82bc7c96aaac8825deb6ce31bed788cb2
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2023-12-28 02:48:03 +0300

    prod card markup, js, styles

--- src/js/product-card.js
+++ src/js/product-card.js
@@ -1,8 +1,5 @@
-import * as fn from "./modules/product-card.js";
 import "./modules/product-card-global.js";
 
 addEventListener("DOMContentLoaded", () => {
-	fn.productAmount();
-	fn.productAllPhotosShow();
 	productBlockCollapseHandler();
 });
