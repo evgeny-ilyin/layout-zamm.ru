6b3762e4a3e47b835858b55afef4383344510844
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2024-02-03 23:16:35 +0300

    change json to post

--- ajax/fetch-more-products.php
+++ ajax/fetch-more-products.php
@@ -1,22 +1,27 @@
 <?
 try {
-	usleep(350000);
-
-	$json = file_get_contents('php://input');
-	$data = json_decode($json, true);
+	usleep(150000);
 	$rand = mt_rand(1192335, 9999999);
 
+	$resp = array('status' => true);
+
 	$url = 'https://deviart.ru/zamm/fetch-filter-chunks.php?more-url-rand-' . $rand;
-	$resp = array('url' => $url);
 
-	$moreProducts = file_get_contents('template-more-products.html');
+	$content = file_get_contents('template-more-products.html');
 	$newPagination = file_get_contents('template-more-pagination.html');
-
 
 	$chunks = array();
 	$chunks['filter-pagination'] = $newPagination;
 
-	$resp = array('status' => true, 'url' => $url, 'products' => $moreProducts, 'chunks' => $chunks);
+
+	if ($url)
+		$resp['url'] = $url;
+
+	if ($chunks)
+		$resp['chunks'] = $chunks;
+
+	$resp['content'] = $content;
+	$resp['reinit'] = 'filter';
 
 	header('Content-Type: application/json; charset=UTF-8');
 	header('HTTP/1.1 200');
