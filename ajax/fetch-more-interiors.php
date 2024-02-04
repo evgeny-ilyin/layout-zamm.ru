45afe3bdc2745754e37b6643987d7c7c3190ac8f
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2024-02-04 18:01:59 +0300

    load more fetcher

---
+++ ajax/fetch-more-interiors.php
@@ -0,0 +1,24 @@
+<?
+try {
+	usleep(150000);
+
+	$resp = array('status' => true);
+	$content = file_get_contents('template-more-interiors.html');
+
+	if ($chunks)
+		$resp['chunks'] = $chunks;
+
+	if ($content)
+		$resp['content'] = $content;
+
+	if ($nocache)
+		$resp['nocache'] = $nocache;
+
+	header('Content-Type: application/json; charset=UTF-8');
+	header('HTTP/1.1 200');
+	print json_encode($resp);
+} catch (Exception $e) {
+	header('Content-Type: application/json; charset=UTF-8');
+	header('HTTP/1.1 500 Internal Server Error');
+	die(json_encode(array('status' => 'error')));
+}
