48246ad792ef8a9ffe774e48543ed1a471dbead7
Evgeny Ilyin <evgeniy.v.ilyin@gmail.com>
2024-10-03 20:20:06 +0300

    Update 2024-10-03T17:20:01.576Z

---
+++ js/fancybox-init.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/modules/fancybox-init.js
+if (!window.fancyboxInit) {
+	window.fancyboxInit = () => {
+		Fancybox.bind("[data-fancybox]", {});
+	}
+}
+
+;// CONCATENATED MODULE: ./src/js/fancybox-init.js
+
+
+addEventListener("DOMContentLoaded", () => {
+	fancyboxInit();
+});
+
+/******/ })()
+;
